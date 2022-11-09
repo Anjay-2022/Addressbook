dc04aea540c0f506c3bb0d2738af7344b9d968ee
Anjay-2022 <anjay2013@gmail.com>
2022-11-09 19:02:25 +0530

    first commit

---
+++ Address_book.java
@@ -0,0 +1,10 @@
+package adressbook;
+
+public class Address_book {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program in Address Book Main class");
+
+	}
+
+}
